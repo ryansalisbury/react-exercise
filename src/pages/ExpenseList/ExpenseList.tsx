0154b1fd98e8c3d976f029a4c8478d2846d86009
Ryan Salisbury <rxs1035@student.bham.ac.uk>
2025-03-05 09:48:59 +0000

    basic data rendering in mui data-table

--- src/pages/ExpenseList/ExpenseList.tsx
+++ src/pages/ExpenseList/ExpenseList.tsx
@@ -1,9 +1,26 @@
+import { Skeleton, Stack } from "@mui/material";
 import { useGetExpenseData } from "./hooks/useGetExpenseData/useGetExpenseData";
+import { useGetExpenseListColumns } from "./hooks/useGetExpenseListColumns/useGetExpenseListColumns";
+import MUIDataTable from "mui-datatables";
 
 const ExpenseList: React.FC = () => {
-  const { data } = useGetExpenseData();
-  console.log(data);
-  return <>Some Data Table rendered here</>;
+  const { data, isLoading } = useGetExpenseData();
+  const columns = useGetExpenseListColumns();
+
+  return (
+    <Stack padding={10}>
+      <MUIDataTable
+        title="Expenses"
+        data={isLoading ? [] : data}
+        columns={columns}
+      />
+      {isLoading && (
+        <div className="table-overlay">
+          <Skeleton variant="rectangular" width="100%"></Skeleton>
+        </div>
+      )}
+    </Stack>
+  );
 };
 
 export default ExpenseList;
--- src/pages/ExpenseList/hooks/useGetExpenseListColumns/useGetExpenseListColumns.ts
+++ src/pages/ExpenseList/hooks/useGetExpenseListColumns/useGetExpenseListColumns.ts
@@ -1,3 +1,33 @@
+import { useMemo } from "react";
+
+const columns = [
+  {
+    name: "id",
+    label: "ID",
+    options: { filter: true },
+  },
+  {
+    name: "date",
+    label: "Date",
+    options: { filter: true },
+  },
+  {
+    name: "amount",
+    label: "Amount",
+    options: { filter: true },
+  },
+  {
+    name: "merchant",
+    label: "Merchant",
+    options: { filter: true },
+  },
+  {
+    name: "category",
+    label: "Category",
+    options: { filter: true },
+  },
+];
+
 export const useGetExpenseListColumns = () => {
-  return "some mui data table columns";
+  return useMemo(() => columns, []);
 };
